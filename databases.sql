36df4519c265078a1b57216d400f9313d6cddab5
Juwendays <juwendays@gmail.com>
2021-08-04 18:53:15 +0700

    Change database name

--- databases.sql
+++ databases.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v9.50 
 MySQL - 5.5.16 : Database - simpak
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -12,9 +13,9 @@ MySQL - 5.5.16 : Database - simpak
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`simpak` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shopping` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `simpak`;
+USE `shopping`;
 
 /*Table structure for table `tbl_ju_detail` */
 
